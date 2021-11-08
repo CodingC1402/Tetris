a66b8f792a5feb9694cebd0f5aedd63cf2ed2b2e
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-11-09 02:47:12 +0700

    whatev

---
+++ Tetris/Sound/Sound.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Text;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace Tetris.Sound
+{
+    public class Sound
+    {
+        public readonly string ID;
+
+
+        static Sound()
+        {
+            var wo = new WaveOutEvent();
+            var af = new AudioFileReader(@"example.mp3");
+            wo.Init(af);
+            wo.Pla
+        }
+
+        private Sound(string id)
+        {
+            ID = id;
+
+        }
+
+        public void Play()
+        {
+            _soundPlayer.Play();
+        }
+    }
+}
